f43a303a76ad89fbf09b24a8d4c52b44c5fa8966
Semesta11 <Ilhamlangitt11@gmail.com>
2026-09-21 04:30:26 +0700

    Labwork 01 - Prelab

---
+++ lw01/prelab/ColourPrint.java
@@ -0,0 +1,24 @@
+public class ColourPrint extends PrintJob {
+
+    public ColourPrint(String id, int pages) {
+        super(id, pages);
+    }
+
+    @Override
+    public int calculateCharge() {
+        int charge;
+
+        if (getPages() <= 10) {
+            charge = getPages() * 1500;
+        } else {
+            charge = (10 * 1500) + ((getPages() - 10) * 1000);
+        }
+
+        return charge + 2000;
+    }
+
+    @Override
+    public String label() {
+        return "Colour";
+    }
+}
